87e0f12142432a1b7e95c8c03591cb8ec3b93446
Christopher <edesonchristopher@gmail.com>
2025-03-21 21:15:11 +0100

    Update: [W03] Implement Dynamic Header and Footer

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -2,20 +2,23 @@ function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error("Bad response from server");
   }
 }
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    // The JSON file should be in public/json/
+    this.path = `/json/${this.category}.json`;
   }
+
   getData() {
     return fetch(this.path)
       .then(convertToJson)
       .then((data) => data);
   }
+
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);

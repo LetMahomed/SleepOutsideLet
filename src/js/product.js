c8210759bf865547b9e41e6ed4a174e7304a4c8a
rachealkatono <rachealktn29@gmail.com>
2025-03-25 13:25:54 +0300

    racheal

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,9 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getLocalStorage("product");
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
